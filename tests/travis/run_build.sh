6cb15a50a4d26a52b63e594d401366b9de9d5c2d
Mark Guinn <mark@adaircreative.com>
2015-06-12 15:49:23 +0100

    Experimenting with code climate

--- tests/travis/run_build.sh
+++ tests/travis/run_build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 if [ "$COVERAGE" = "1" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-	vendor/bin/phpunit -c shop/phpunit.xml.dist --testsuite Split1 --coverage-clover shop/coverage.xml
-elif [ "$COVERAGE" = "2" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-	vendor/bin/phpunit -c shop/phpunit.xml.dist --testsuite Split2 --coverage-clover shop/coverage.xml
-elif [ "$COVERAGE" = "3" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-	vendor/bin/phpunit -c shop/phpunit.xml.dist --testsuite Split3 --coverage-clover shop/coverage.xml
+	vendor/bin/phpunit -c shop/phpunit.xml.dist --coverage-clover build/log/coverage.xml
+#if [ "$COVERAGE" = "1" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+#	vendor/bin/phpunit -c shop/phpunit.xml.dist --testsuite Split1 --coverage-clover shop/coverage.xml
+#elif [ "$COVERAGE" = "2" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+#	vendor/bin/phpunit -c shop/phpunit.xml.dist --testsuite Split2 --coverage-clover shop/coverage.xml
+#elif [ "$COVERAGE" = "3" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+#	vendor/bin/phpunit -c shop/phpunit.xml.dist --testsuite Split3 --coverage-clover shop/coverage.xml
 else
 	vendor/bin/phpunit -c shop/phpunit.xml.dist
 fi
