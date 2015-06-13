c718a9861020ed26206318891b9e78444ae534b7
Mark Guinn <mark@adaircreative.com>
2015-06-13 21:14:50 +0100

    still trying for code coverage 3

--- tests/travis/run_build.sh
+++ tests/travis/run_build.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env sh
-if [ "$COVERAGE" > 0 ]; then
-	echo "Running coverage..."
+#if [ "$COVERAGE" > 0 ]; then
+#	echo "Running coverage..."
+#	mkdir ~/builds/ss/shop/build
+#	mkdir ~/builds/ss/shop/build/logs
+#	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --coverage-clover ~/builds/ss/shop/build/logs/clover.xml
+if [ "$COVERAGE" = 1 ]; then
 	mkdir ~/builds/ss/shop/build
 	mkdir ~/builds/ss/shop/build/logs
-	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --coverage-clover ~/builds/ss/shop/build/logs/clover.xml
-#if [ "$COVERAGE" = "1" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-#	vendor/bin/phpunit -c shop/phpunit.xml.dist --testsuite Split1 --coverage-clover shop/coverage.xml
+	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split1 --coverage-clover ~/builds/ss/shop/build/logs/clover.xml
 #elif [ "$COVERAGE" = "2" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-#	vendor/bin/phpunit -c shop/phpunit.xml.dist --testsuite Split2 --coverage-clover shop/coverage.xml
-#elif [ "$COVERAGE" = "3" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-#	vendor/bin/phpunit -c shop/phpunit.xml.dist --testsuite Split3 --coverage-clover shop/coverage.xml
+elif [ "$COVERAGE" = "2" ]; then
+	mkdir ~/builds/ss/shop/build
+	mkdir ~/builds/ss/shop/build/logs
+	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split2 --coverage-clover ~/builds/ss/shop/build/logs/clover.xml
+elif [ "$COVERAGE" = "3" ]; then
+	mkdir ~/builds/ss/shop/build
+	mkdir ~/builds/ss/shop/build/logs
+	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split3 --coverage-clover ~/builds/ss/shop/build/logs/clover.xml
 else
 	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist
 fi
