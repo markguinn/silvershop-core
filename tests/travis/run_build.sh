6d71cc344418619231a6c198cb6d592b67724f03
Mark Guinn <mark@adaircreative.com>
2015-06-15 15:27:35 +0100

    trying out coveralls

--- tests/travis/run_build.sh
+++ tests/travis/run_build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 #if [ "$COVERAGE" = "1" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
 if [ "$COVERAGE" = "1" ]; then
-	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split1 --coverage-clover ~/builds/ss/shop/coverage.xml
+	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split1 --coverage-clover ~/builds/ss/shop/coverage1.xml
 elif [ "$COVERAGE" = "2" ]; then
-	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split2 --coverage-clover ~/builds/ss/shop/coverage.xml
+	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split2 --coverage-clover ~/builds/ss/shop/coverage2.xml
 elif [ "$COVERAGE" = "3" ]; then
-	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split3 --coverage-clover ~/builds/ss/shop/coverage.xml
+	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split3 --coverage-clover ~/builds/ss/shop/coverage3.xml
 else
 	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist
 fi
