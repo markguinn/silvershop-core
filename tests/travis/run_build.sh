344b1656a0c5fc040ac994d0ecd8d8e860a2cfb2
Mark Guinn <mark@adaircreative.com>
2015-06-16 00:34:40 +0100

    trying codecov 4

--- tests/travis/run_build.sh
+++ tests/travis/run_build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 #if [ "$COVERAGE" = "1" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-if [ "$COVERAGE" = 1 ]; then
-	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split1 --coverage-clover /home/travis/builds/ss/shop/coverage1.xml
-elif [ "$COVERAGE" = 2 ]; then
-	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split2 --coverage-clover /home/travis/builds/ss/shop/coverage2.xml
-elif [ "$COVERAGE" = 3 ]; then
-	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split3 --coverage-clover /home/travis/builds/ss/shop/coverage3.xml
+if [ "$COVERAGE" = "1" ]; then
+	vendor/bin/phpunit -c shop/phpunit.xml.dist --testsuite Split1 --coverage-clover=~/coverage.xml
+elif [ "$COVERAGE" = "2" ]; then
+	vendor/bin/phpunit -c shop/phpunit.xml.dist --testsuite Split2 --coverage-clover=coverage2.xml
+elif [ "$COVERAGE" = "3" ]; then
+	vendor/bin/phpunit -c shop/phpunit.xml.dist --testsuite Split3 --coverage-clover=shop/coverage3.xml
 else
-	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist
+	vendor/bin/phpunit -c shop/phpunit.xml.dist
 fi
