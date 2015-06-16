7a9ba1093cdb52243bd811046be2fc9b656954b7
Mark Guinn <mark@adaircreative.com>
2015-06-16 13:43:51 +0100

    hopefully fixing scrutinizer cancelation issue

--- tests/travis/run_build.sh
+++ tests/travis/run_build.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env sh
 #if [ "$COVERAGE" = "1" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-if [ "$COVERAGE" > 0 ]; then
-	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --coverage-clover ~/builds/ss/shop/coverage.xml
-#if [ "$COVERAGE" = "1" ]; then
-#	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split1 --coverage-clover ~/builds/ss/shop/coverage.xml
-#elif [ "$COVERAGE" = "2" ]; then
-#	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split2 --coverage-clover ~/builds/ss/shop/coverage.xml
-#elif [ "$COVERAGE" = "3" ]; then
-#	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split3 --coverage-clover ~/builds/ss/shop/coverage.xml
+if [ "$COVERAGE" = "1" ]; then
+	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split1 --coverage-clover ~/builds/ss/shop/coverage.xml
+elif [ "$COVERAGE" = "2" ]; then
+	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split2 --coverage-clover ~/builds/ss/shop/coverage.xml
+elif [ "$COVERAGE" = "3" ]; then
+	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist --testsuite Split3 --coverage-clover ~/builds/ss/shop/coverage.xml
 else
 	vendor/bin/phpunit -c ~/builds/ss/shop/phpunit.xml.dist
 fi
