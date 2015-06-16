e34a4a25b34c7bf60fcad88999e636e00f580bf5
Mark Guinn <mark@adaircreative.com>
2015-06-16 14:37:45 +0100

    timeout fixes

--- tests/travis/upload_coverage.sh
+++ tests/travis/upload_coverage.sh
@@ -2,5 +2,6 @@
 echo "coverage = $COVERAGE"
 if [ -n "$COVERAGE" ]; then
 	cd shop
-	../vendor/bin/ocular code-coverage:upload -v --format=php-clover ~/builds/ss/shop/coverage.xml
+	wget https://scrutinizer-ci.com/ocular.phar
+	php ocular.phar code-coverage:upload -v --format=php-clover ~/builds/ss/shop/coverage.xml
 fi
